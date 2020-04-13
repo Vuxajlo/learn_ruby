62c29621a57e663c0757e0dcecb2bcdf5357b84a
Vuk <vukmijatovic@gmail.com>
2020-04-13 14:31:07 +0200

    tasks completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,20 @@
-#write your code here
+def echo word 
+	word
+end
+def shout words
+	words.upcase
+end
+
+def repeat words, number = 2
+       answer = ""  
+
+	number.times do 
+	       answer = answer + words + " "  
+end
+	answer.strip
+end
+def start_of_word word, n
+
+	word[0,n]
+end
+
